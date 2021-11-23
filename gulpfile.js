4cbf8b27f32deeb7322cbf82056bc116e2b646bc
Oscar Cantor <ocantorb1889@gmail.com>
2021-11-23 13:33:10 +0100

    Creando en gulp formato de imagenes avif

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css( done ){
 
@@ -42,6 +43,18 @@ function versionWebp( done ) {
   done();
 }
 
+function versionAvif( done ) {
+
+  const opciones = {
+    quality: 50
+  };
+
+  src('src/img/**/*.{png,jpg}') // Le indico ubicacion de las imagenes y que extencion de imagenes puede convertir
+  .pipe( avif( opciones ) ) // Realiza la conversion
+  .pipe( dest( 'build/img') ) // Destino donde las almacenara
+  done();
+}
+
 function dev(done) {
 
   watch('src/scss/**/*.scss', css) // Toma dos parametros 1-A que archivo le voy a hacer watch 2-Que funcion va a estar asociada
@@ -51,6 +64,7 @@ function dev(done) {
 
 exports.css = css;
 exports.imagenes = imagenes;
-exports.versionWebp = versionWebp; // Ponemos disponoble nuestra funcion en el gulp
-exports.dev = parallel( imagenes, versionWebp, dev ); // Por el parallel va a ejecutar las dos funciones
+exports.versionWebp = versionWebp; // Ponemos disponible nuestra funcion en el gulp
+exports.versionAvif = versionAvif; // Ponemos disponible nuestra funcion en el gulp
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev ); // Por el parallel va a ejecutar las dos funciones
 
