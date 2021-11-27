0e802b47a4003a1b2c300a8be9bf6523845d1066
Oscar Cantor <ocantorb1889@gmail.com>
2021-11-27 14:58:59 +0100

    Se crea galeria de fotos en JS trabajando DOM

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-alert('Holaaa...');
+document.addEventListener('DOMContentLoaded', function() { // Vamos a escuchar nuestra aplicacion
+  iniciarApp();
+});
+
+function iniciarApp() {
+  crearGaleria();
+}
+
+function crearGaleria() {
+  const galeria = document.querySelector('.galeria-imagenes');
+
+  for( let i = 1; i <= 12; i++ ) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = ` 
+      <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+      <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+      <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen galeria">
+     `; 
+    galeria.appendChild(imagen);
+  }
+}
