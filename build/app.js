33ff9b828a380c26b91f4ad9459438a39c207492
Oscar Cantor <ocantorb1889@gmail.com>
2021-11-30 02:21:52 +0100

    Se trabaja en la logica y estilos de la ventana modal de la galeria

--- build/app.js
+++ build/app.js
@@ -10,7 +10,7 @@ function crearGaleria() {
   const galeria = document.querySelector('.galeria-imagenes');
 
   for( let i = 1; i <= 12; i++ ) {
-    const imagen = document.createElement('picture');
+    const imagen = document.createElement('PICTURE');
     imagen.innerHTML = ` 
       <source srcset="build/img/thumb/${i}.avif" type="image/avif">
       <source srcset="build/img/thumb/${i}.webp" type="image/webp">
@@ -25,6 +25,18 @@ function crearGaleria() {
 
 function mostrarImagen( id ) { // Lo utilizaremos para que nos indique a que foto le estamos dando click
 
-  console.log('mostrando...', id);
+  const imagen = document.createElement('PICTURE');
+    imagen.innerHTML = ` 
+      <source srcset="build/img/grande/${id}.avif" type="image/avif">
+      <source srcset="build/img/grande/${id}.webp" type="image/webp">
+      <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen galeria">
+     `;
+
+  const overlay = document.createElement('DIV'); // Creo el div y se lo asigno al overlay
+  overlay.appendChild(imagen); // A ese DIV le agrego como hijo a imagen
+  overlay.classList.add('overlay') // Agrego uma clase llamada overlay para poder darle estilos con css 
+
+  const body = document.querySelector('body'); // Selecciono todo el body 
+  body.appendChild(overlay) // Le agrego el overlay al body para que agrege la imagen y la muestre
 }
 
